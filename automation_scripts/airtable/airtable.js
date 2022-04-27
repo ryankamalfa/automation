59561624bca42af2ae53a12e7f06274f3a81d381
Your Name <you@example.com>
2022-04-27 14:54:52 +0200

    a

--- automation_scripts/airtable/airtable.js
+++ automation_scripts/airtable/airtable.js
@@ -13,9 +13,18 @@ const arango = require('./model/arango');
             Replace into ArangoDB
         */ 
         
+
+        let scriptSettings = await arango.query(`
+            For o in script_settings
+            Filter o.type == 'config'
+            return o
+            `);
+        let scriptSettingsData = await scriptSettings.all();
+
+        
         let listings =  await arango.query(`For listing in crawled_listings
                         Sort listing.created_at asc
-                        Filter listing.manheim and listing.vin and !listing.airtable and listing.price or listing.start_price  and listing.US_base_mmr
+                        Filter listing.manheim and listing.vin and !listing.airtable and listing.price or listing.start_price  and listing.US_base_mmr ${scriptSettingsData[0].autotrader.enable ? " and listing.platform == 'autotrader'" : ''} ${scriptSettingsData[0].adesa.enable ? " and listing.platform == 'adesa'" : ''}
                         return {listing_id:listing.listing_id,platform:listing.platform,
                             search_trim:listing.search_trim,
                             search_make:listing.search_make,
