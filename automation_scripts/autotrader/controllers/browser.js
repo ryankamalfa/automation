ebbf1ecfc11146b5de8647eb333b015f81810c51
Your Name <you@example.com>
2022-04-28 11:13:38 +0200

    A

--- automation_scripts/autotrader/controllers/browser.js
+++ automation_scripts/autotrader/controllers/browser.js
@@ -20,7 +20,7 @@ class Browser {
             }
             this.browser = await puppeteer.launch({
                 executablePath: '/usr/bin/google-chrome',
-                headless: false,
+                headless: true,
                 ignoreHTTPSErrors: true,
                 args: [
                     '--no-sandbox',
